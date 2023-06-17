d1112a272720f42f782f579343beeadffef42037
k1au3 <lindquistconcern@gmail.com>
2023-06-17 20:14:50 +0300

    destinations

--- src/components/destination.js
+++ src/components/destination.js
@@ -2,10 +2,12 @@
 
 function Gallery ({ darasa, title, sub, rate}){
     return(
-        <div className={darasa}>
-                <span>{rate}</span>
+        <div className={darasa} id="card">
+            <span>{rate}</span>
+            <div className="text">
                 <h3>{title}</h3>
-            <h5>{sub}</h5>
+                <h5>{sub}</h5>
+            </div>
         </div>
     );
 }
@@ -15,15 +17,15 @@ const Destination = () => {
         <div className="destinations">
             <div className="nav">
                 <div className="left">
-                    <h3>Top Destinaions</h3>
+                    <h3>Top Destinations</h3>
                     <span>Some Brilliant reasons travelog should be your one-stop-shop</span>
                 </div>
                 <button>Check All</button>
             </div>
             <div className="gallery">
                 <div className="left">
-                    <Gallery darasa="vietnam" rate="4.5" title="Vietnam" sub="Culture" />
-                    <Gallery darasa="vietnam" rate="4.2" title="Old Rain" sub="Train track" />
+                    <Gallery  darasa="vietnam" rate="4.5" title="Vietnam" sub="Culture" />
+                    <Gallery darasa="train" rate="4.2" title="Old Rain" sub="Train track" />
                 </div>
                 
                 <Gallery darasa="mid" rate="4.7" title="Greece" sub="Beaches" />
@@ -32,7 +34,7 @@ const Destination = () => {
                     <Gallery darasa="london" rate="4.2" title="London" sub="Kingdom" />
                     <div className="bottom">
                         <Gallery darasa="kenya" rate="4.9" title="Kenya" sub="Safari" />
-                        <Gallery darasa="Paris" rate="4.7" title="Paris" sub="Eiffel" />
+                        <Gallery darasa="paris" rate="4.7" title="Paris" sub="Eiffel" />
                     </div>
                 </div>
                 
